6d55ed5d6d9e2ed49bec21a168a49bad2253873a
James Smith Simbolon <2582019@unai.edu>
2026-09-14 10:02:52 +0700

    Masukan NIK

---
+++ src/KartuMahasiswa.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class KartuMahasiswa {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("NIK (16 digit)             : ");
+        long nik = scanner.nextLong();
+
+
+
+
+    }
+}
