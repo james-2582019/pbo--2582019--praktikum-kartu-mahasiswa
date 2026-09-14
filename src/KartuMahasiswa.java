a4b617bf01d6b18b8ac402d768c449cdc1f27541
James Smith Simbolon <2582019@unai.edu>
2026-09-14 10:08:22 +0700

    tinggi badan

--- src/KartuMahasiswa.java
+++ src/KartuMahasiswa.java
@@ -9,6 +9,24 @@ public class KartuMahasiswa {
         System.out.print("NIK (16 digit)             : ");
         long nik = scanner.nextLong();
 
+        System.out.print("Umur                       : ");
+        int umur = scanner.nextInt();
+
+        System.out.print("Tinggi badan (cm)          : ");
+        double tinggiBadan = scanner.nextDouble();
+
+        System.out.print("Golongan darah             : ");
+        char golonganDarah = scanner.next().charAt(0);
+
+        System.out.print("Sudah menikah (true/false) : ");
+        boolean sudahMenikah = scanner.nextBoolean();
+
+
+        scanner.nextLine();
+
+        System.out.print("Nama lengkap         : ");
+        String namaLengkap = scanner.nextLine();
+
 
 
 
